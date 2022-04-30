aa94fd2815335f08d2ded922b8b135e6a42bdb14
Dũng Hoàng <hoangtrungdungxz@gmail.com>
2022-04-30 18:57:20 +0700

    sale_agent

--- src/admin/Controllers/SaleAgentController.php
+++ src/admin/Controllers/SaleAgentController.php
@@ -15,11 +15,16 @@ class SaleAgentController  extends AdminControllers
 {
     protected $customerResourceModel;
     private $sale_agent_show = '';
+
+    private $commission_from_child = 0;
+    private $child_agents = [];
+
     function __construct()
     {
         parent::__construct();
         $this->customerResourceModel =  new CustomerResourceModel();
     }
+
     /**
      * Index
      * 
@@ -27,18 +32,18 @@ class SaleAgentController  extends AdminControllers
      */
     function index()
     {
-        // $customers = $this->customerResourceModel
-        //     ->select('customers.*, c2. name as superior_agent_name')
-        //     ->join('customers c2', 'customers.superior_agent_id = c2.id', 'LEFT OUTER JOIN')
-        //     ->whereNot('customers.superior_agent_id', null, 'AND', '<=>')
-        //     ->getAll();
-        // $this->with($customers);
-
         $sale_agents = $this->customerResourceModel
-            ->select('customers.*,SUM(orderdetails.price) as sum_price')
+            ->select('customers.*,SUM(orderdetails.price * 20 / 100) as sum_price')
             ->join('orderdetails', 'orderdetails.agent_id = customers.id', 'LEFT OUTER JOIN')
             ->groupBy('customers.id')
             ->getAll();
+
+
+        foreach ($sale_agents as $key => $s) {
+
+            $this->findParent($sale_agents, $s->getSuperior_agent_id(), $s->sum_price / 2);
+        }
+
 
         $this->showSaleAgent($sale_agents);
 
@@ -70,10 +75,10 @@ class SaleAgentController  extends AdminControllers
                         </td>
                         <td><a href="mailto:' . $s->getEmail() . '">' . $s->getEmail() . '</a></td>
 
-                       <td>' . number_format($s->sum_price * 20 / 100) . ' ₫</td>
+                       <td>' . number_format($s->sum_price) . ' ₫</td>
 
                         <td class="text-right">
-                            <a href="' . WEBROOT . 'admin/saleagent/detaild/sid/' . $s->getId() . '" class="btn btn-sm bg-success-light mr-2">
+                            <a href="' . WEBROOT . 'admin/saleagent/detail/sid/' . $s->getId() . '" class="btn btn-sm bg-success-light mr-2">
                                 <i class="far fa-edit mr-1"></i> Chi tiết
                             </a>
                         </td>
@@ -82,6 +87,112 @@ class SaleAgentController  extends AdminControllers
                 unset($sale_agents[$key]);
 
                 $this->showSaleAgent($sale_agents, $s->getId(), $char . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+            }
+        }
+    }
+
+
+    private function findParent(&$sale_agents, $superior_agent_id = 0, $sum_price = 0)
+    {
+        $customer = $this->findInList($sale_agents, $superior_agent_id);
+
+        if ($superior_agent_id != 0 && $superior_agent_id != null) {
+
+            $customer->sum_price = $customer->sum_price + $sum_price;
+
+            $this->findParent($sale_agents, $customer->getSuperior_agent_id(), $sum_price = $sum_price / 2);
+        }
+    }
+
+
+    private function findInList(&$array, $id)
+    {
+        foreach ($array as $key => $value) {
+            if ($value->getId() == $id) {
+                return $value;
+            }
+        }
+    }
+
+
+    /**
+     * Index
+     * 
+     * @param AcctionName Chi tiết đại lý
+     */
+    function detail($params)
+    {
+        if (!isset($params['sid'])) {
+            header('Location: ' . WEBROOT . "admin/saleagent");
+            die;
+        }
+        $sale_agents = $this->customerResourceModel
+
+            ->getAll();
+
+
+        $sale_agent = $this->customerResourceModel
+            ->select('customers.*,SUM(orderdetails.price * 20 / 100) as sum_price')
+            ->join('orderdetails', 'orderdetails.agent_id = customers.id', 'LEFT OUTER JOIN')
+            ->where('customers.id', $params['sid'])
+            ->groupBy('customers.id')
+            ->get();
+
+        $sale_success = $this->customerResourceModel
+            ->join('orderdetails', 'orderdetails.agent_id = customers.id')
+
+            ->join('products', 'products.id=orderdetails.product_id')
+
+            ->where('customers.id', $params['sid'])
+
+            ->join('orders', 'orders.id=orderdetails.order_id')
+
+            ->where('orders.status', 4)
+
+            ->select('customers.*,orderdetails.price * 20 / 100 as commission,
+            orders.status as  orders_status, orders.date as  orders_date,
+            products.name as products_name')
+            ->getAll();
+
+        $this->countCommissionFromChild($sale_agents, $sale_agent->getId(), 1);
+
+        $commission_from_child = $this->commission_from_child;
+
+
+        $this->with($sale_success);
+
+        $this->with($sale_agent);
+
+        $this->with($commission_from_child);
+
+        $child_agents = $this->child_agents;
+        $this->with($child_agents);
+
+        $this->render("detail");
+    }
+
+    private function countCommissionFromChild($sale_agents, $superior_agent_id = 0, $lv = 1)
+    {
+        $lv = $lv * 2;
+        foreach ($sale_agents as $key => $s) {
+
+            // Nếu là chuyên mục con thì hiển thị
+            if ($s->getSuperior_agent_id() == $superior_agent_id && $s->getSuperior_agent_id() != null) {
+
+                $sale_agent = $this->customerResourceModel
+                    ->select('customers.*,SUM(orderdetails.price * 20 / 100) / ' . $lv . ' as sum_price')
+                    ->join('orderdetails', 'orderdetails.agent_id = customers.id', 'LEFT OUTER JOIN')
+                    ->where('customers.id', $s->getId())
+                    ->groupBy('customers.id')
+                    ->get();
+
+                $this->commission_from_child += $sale_agent->sum_price;
+
+                array_push($this->child_agents,  $sale_agent);
+
+                unset($sale_agents[$key]);
+
+                $this->countCommissionFromChild($sale_agents, $s->getId(), $lv);
             }
         }
     }
---
+++ src/admin/Views/SaleAgent/detail.php
@@ -0,0 +1,111 @@
+<div class="page-wrapper">
+    <div class="content container-fluid">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="row">
+                <div class="col">
+                    <h3 class="page-title">Thông tin đại lý</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-center mb-0 ">
+
+                                <tr>
+                                    <th style="width:150px;">Họ tên</th>
+                                    <th><?= $sale_agent->getName(); ?></th>
+                                </tr>
+                                <tr>
+                                    <th style="width:150px;">Email</th>
+                                    <th><?= $sale_agent->getEmail(); ?></th>
+                                </tr>
+                                <tr>
+                                    <th style="width:150px;">Địa chỉ</th>
+                                    <th><?= $sale_agent->getAddress(); ?></th>
+                                </tr>
+                                <tr>
+                                    <th style="width:150px;">Điện thoại</th>
+                                    <th><?= $sale_agent->getPhone(); ?></th>
+                                </tr>
+                                <tr>
+                                    <th style="width:150px;">Lợi nhuận từ bán hàng</th>
+                                    <th><?= number_format($sale_agent->sum_price); ?> ₫</th>
+                                </tr>
+                                <tr>
+                                    <th style="width:150px;">Lợi nhuận từ đại lý dưới quyền</th>
+                                    <th><?= number_format($commission_from_child); ?> ₫</th>
+                                </tr>
+
+
+                            </table>
+
+                        </div>
+                        <div class="panel-footer"><a href="#" onclick="history.go(-1);" class="btn btn-primary">Quay lại</a></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4>Hàng bán được</h4>
+                        <div class="table-responsive">
+                            <table class="table table-hover table-center mb-0 datatable">
+                                <thead>
+                                    <tr>
+                                        <th>Sản phẩm</th>
+                                        <th>Lợi nhuận</th>
+                                        <th>Ngày đặt</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($sale_success as $sa) : ?>
+                                        <tr>
+
+                                            <td><?= $sa->products_name; ?></td>
+                                            <td><?= number_format($sa->commission); ?> ₫</td>
+                                            <td><?= $sa->orders_date; ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h4>Lợi nhuận từ đại lý dưới quyền</h4>
+                        <div class="table-responsive">
+                            <table class="table table-hover table-center mb-0 datatable">
+                                <thead>
+                                    <tr>
+                                        <th>Tên</th>
+                                        <th>Email</th>
+                                        <th>Lợi nhuận</th>
+                                    </tr>
+                                    </ <thead>
+                                <tbody>
+                                    <?php foreach ($child_agents as $sa) : ?>
+                                        <tr>
+                                            <td><?= $sa->getName(); ?></td>
+                                            <td><?= $sa->getEmail(); ?></td>
+                                            <td><?= number_format($sa->sum_price); ?> ₫</td>
+                                        </tr>
+                                    <?php endforeach; ?>
+
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
