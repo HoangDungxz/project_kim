69079ca98b9e860561c21fbb08a27d5201c0f1da
Dũng Hoàng <hoangtrungdungxz@gmail.com>
2022-05-07 20:24:05 +0700

    news d

--- src/Views/News/detail.php
+++ src/Views/News/detail.php
@@ -1,7 +1,7 @@
 <div class="middle col-md-9">
     <!-- chi tiet -->
     <h3><?= $news->getName(); ?></h3>
-    <img src="assets/upload/news/<?= $news->getPhoto(); ?>" style="width:100%;">
+    <img src="<?= PUBLIC_URL ?>upload/news/<?= $news->getPhoto(); ?>" style="width:100%;">
     <p><?= $news->getDescription(); ?></p>
     <p><?= $news->getContent(); ?></p>
     <!-- chi tiet -->
